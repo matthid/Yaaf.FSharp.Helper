383506de4e0770ac7565621434a31ba0847c9abb
Matthias Dittrich <matthi.d@googlemail.com>
2015-03-09 19:34:31 +0100

    add Async.reraise .

--- src/source/Yaaf.FSharp.Helper/Helper.fs
+++ src/source/Yaaf.FSharp.Helper/Helper.fs
@@ -145,6 +145,9 @@ module Async =
     let zip a = async { let! b = a
                         let! c = b
                         return c }
+
+    let reraise e = Task.reraisePreserveStackTrace e
+
 [<AutoOpen>]
 module AsyncExtensions =
 
